b43a065a7ee28729d3963b4273ad804f6008403f
R3enox <129979133+R3enox@users.noreply.github.com>
2023-12-07 07:48:20 +0200

    update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import "./dist/baseTypes/1";
+import "./dist/baseTypes/2";
+import "./dist/baseTypes/3";
+import "./dist/baseTypes/4";
+import "./dist/baseTypes/5";
+import "./dist/baseTypes/6";
+import "./dist/baseTypes/7";
+import "./dist/baseTypes/8";
+import "./dist/baseTypes/9";
+
+import "./dist/generic/1";
+import "./dist/generic/2";
+import "./dist/generic/3";
+import "./dist/generic/4";
+import "./dist/generic/5";
+import "./dist/generic/6";
+import "./dist/generic/7";
+import "./dist/generic/8";
